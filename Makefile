8a56ad087016a38f26cd11584f2dde0e325912e7
Eduardo Ibarra <eduardoibarra4242@gmail.com>
2025-01-16 17:58:02 -0700

    Add CI infrastructure.

--- Makefile
+++ Makefile
@@ -2,3 +2,10 @@ all: cu
 
 cu:
 	cd src && $(MAKE)
+
+build_tests:
+	cd tests && $(MAKE)
+
+tests: build_tests
+	clear
+	bin/tests/scanner.test
---
+++ tests/scanner.test.c
@@ -0,0 +1,15 @@
+#include <unity.h>
+#include <stdbool.h>
+
+void setUp(void) {
+  // set stuff up here
+}
+
+void tearDown(void) {
+  // clean stuff up here
+}
+
+void test_booleans(void) {
+  TEST_ASSERT_TRUE(true);
+  TEST_ASSERT_FALSE(false);
+}
